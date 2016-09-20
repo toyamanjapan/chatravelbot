3919ee1ceb9c06c33eda27f5ff2cc16e436ef6e4
toyamanjapan <el.scorcho.weezer@gmail.com>
2016-09-20 17:12:06 +0900

    add callback

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,11 @@
 class HelloController < ApplicationController
   def index
   end
+  def callback
+   if params["hub.verify_token"] == "hogehoge"
+    render json: params["hub.challenge"]
+   else
+    render json: "Error, wrong validation token"
+   end
+  end
 end
