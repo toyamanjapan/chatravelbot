1cef67ba9bca5d7041a67d5f88e4a0e4dc20241f
toyamanjapan <el.scorcho.weezer@gmail.com>
2016-09-20 17:51:25 +0900

    add callback

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,32 +1,9 @@
 class HelloController < ApplicationController
   def index
-   token = "<TOKEN>"
-
-   message = params["entry"][0]["messaging"][0]
-
-   if message.include?("message")
-
-      #ユーザーの発言
-
-      sender = message["sender"]["id"]
-      text = message["message"]["text"]
-
-      endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
-      request_content = {recipient: {id:sender},
-      message: {text: text}
-    }
-
-    content_json = request_content.to_json
-
-    RestClient.post(endpoint_uri, content_json, {
-      'Content-Type' => 'application/json; charset=UTF-8'
-      }){ |response, request, result, &block|
-      p response
-      p request
-      p result
-    }
-  else
-      #botの発言
-    end
-  end
+   if params["hub.verify_token"] == "hogehoge"
+    render json: params["hub.challenge"]
+   else
+    render json: "Error, wrong validation token"
+   end
+ end
 end
